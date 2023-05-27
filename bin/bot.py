34c8dbeb0a7b0bfadc9e065f9109f4aed74608bb
peanut <joanne.hsu.peanut@gmail.com>
2023-05-28 02:04:50 +0800

    update pybot
    1. model="gpt-3.5-turbo"
    2. 改 git ignore，金鑰放到key資料夾
    3. 可以輸出字串了

--- bin/bot.py
+++ bin/bot.py
@@ -1,36 +1,38 @@
+# -*- coding: utf-8 -*-
 import openai
 import re
 import sys
 
-# 設定 ChatGPT 的 API 金鑰
-keyfile = open("bin/key.txt", "r")
+keyfile = open("../key/openaiKey.txt", "r")
 key = keyfile.readline()
-#openai.api_key = key
+openai.api_key = key
 
-def query_skills(query):
-    # 使用 ChatGPT 查詢技能
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=f"我想學習{query}，我可以在大學上什麼課？",
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.7
+def skills(user_input):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": f"我想學習{user_input}，我可以在大學上什麼課"}
+        ]
     )
-    
+    result = ''
+    for choice in response.choices:
+        result += choice.message.content
+
     # 從回應中提取技能
     # 從response提取數字加句點開頭並以冒號結尾的課程名稱到gptclass
     pattern = "\d+\.\s*(.*?)："
-    gptclass = re.findall(pattern, response)
+    gptclass = re.findall(pattern, result)
     
     return gptclass
+
 
 # 接收父程式傳遞的輸入參數
 user_input = sys.argv[1] if len(sys.argv) > 1 else None
 
 # 在腳本中使用輸入參數進行處理
 if user_input:
-    result = query_skills(user_input)
+    result = skills(user_input)
 else:
     result = "沒有收到輸入參數"
 
