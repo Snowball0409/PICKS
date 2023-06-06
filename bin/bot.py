08f9e4ee84cfa3916ca9692fb8d948415dbb5dc4
peanut <joanne.hsu.peanut@gmail.com>
2023-06-06 22:34:38 +0800

    azure pybot

--- bin/bot.py
+++ bin/bot.py
@@ -1,48 +1,52 @@
-# -*- coding: utf-8 -*-
-import openai
+import logging
 import re
-import sys
-
-keyfile = open("key/openaiKey.txt", "r")
-key = keyfile.readline()
-openai.api_key = key
-
-def skills(user_input):
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a chatbot"},
-            {"role": "user", "content": f"我想學習{user_input}，我可以在大學上什麼課"}
-        ]
-    )
-    result = ''
-    for choice in response.choices:
-        result += choice.message.content
-
-    # 從回應中提取技能
-    # 從response提取數字加句點開頭並以冒號結尾的課程名稱到gptclass
-    pattern = "\d+\.\s*(.*?)："
-    gptclass = re.findall(pattern, result)
-    
-    return gptclass
+import openai
+import azure.functions as func
+import json
 
 
-# 接收父程式傳遞的輸入參數
-user_input = sys.argv[1] if len(sys.argv) > 1 else None
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    openai.api_key = "" # 你的 openai api key
+    userInput = req.params.get('userInput')
+    if not userInput:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            userInput = req_body.get('userInput')
 
-if user_input:
-    retry  =  5
-    while retry > -1:
-        result = skills(user_input)
-        if result:
-            # 將結果輸出到控制台
-            print(result)
-            break
-        if retry ==  0:
-            print("五次請求失敗")
-            break
-        else: 
-            retry -= 1
-            #print("retry") #測試用
-else:
-    result = "沒有收到輸入參數"
+    if userInput:
+        output = ""
+        # 問gpt五次
+        for i in range(5,-1,-1):
+            if i == 0:
+                output = "五次請求失敗"
+                break
+            openaiResponse = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": "You are a chatbot"},
+                    {"role": "user", "content": f"我想學習{userInput}，我可以在大學上什麼課"}
+                ]
+            )
+            result = ''
+            for choice in openaiResponse.choices:
+                result += choice.message.content
+            # 提取數字加句點開頭並以冒號結尾的課程名稱到 output
+            pattern = "\d+\.\s*(.*?)："
+            output = re.findall(pattern, result) # 課程list
+            if output:
+                break
+        # 包起來
+        response = {
+                    "Input": userInput,
+                    "Output": output
+                }
+        return func.HttpResponse(json.dumps(response), mimetype="application/json")
+    else:
+        return func.HttpResponse(
+            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            status_code=200
+        )
